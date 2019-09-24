6f242aa4321230d65b07046f199b8e8fcdb56265
Alan Eicker <alaneicker@gmail.com>
2019-09-24 15:40:42 -0500

    Update setup.js

--- example/setup.js
+++ example/setup.js
@@ -1,12 +1,33 @@
 import db from './db-connection';
 
 (async () => {
-
-  await db.createTable('Users', {
-    name: 'string',
-    email: 'string',
-  });
-
-  console.log('Table "Users" has been created');
-
+  try {
+    await db.createTable('Users', {
+      name: 'string',
+      email: 'string',
+    });
+  
+    await db.insert('Users', [
+      {
+        name: 'Alan Eicker',
+        email: 'alaneicker@gmail.com',
+      },
+      {
+        name: 'Fred Smith',
+        email: 'f.smith@gmail.com',
+      },
+      {
+        name: 'Paul Newman',
+        email: 'p_newman@yahoo.com',
+      },
+      {
+        name: 'Ted Johnson',
+        email: 'ted_johnson@gmail.com',
+      },
+    ]);
+  
+    console.log('Table "Users" has been created');
+  } catch (err) {
+    console.log(err);
+  }
 })();
