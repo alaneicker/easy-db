7defefe43c8ac15dca796796f89ef7119f1ccbaa
Alan Eicker <alaneicker@hotmail.com>
2019-09-25 10:13:23 -0500

    Update setup.js

--- example/setup.js
+++ example/setup.js
@@ -5,26 +5,39 @@ import db from './db-connection';
     await db.createTable('Users', {
       name: 'string',
       email: 'string',
+      age: 'number',
+      status: 'string',
     });
   
-    await db.insert('Users', [
-      {
-        name: 'Alan Eicker',
-        email: 'alaneicker@gmail.com',
-      },
-      {
-        name: 'Fred Smith',
-        email: 'f.smith@gmail.com',
-      },
-      {
-        name: 'Paul Newman',
-        email: 'p_newman@yahoo.com',
-      },
-      {
-        name: 'Ted Johnson',
-        email: 'ted_johnson@gmail.com',
-      },
-    ]);
+    await db.insert({
+      table: 'Users',
+      records: [
+        {
+          name: 'Alan Eicker',
+          email: 'alaneicker@gmail.com',
+          age: 44,
+          status: 'active',
+        },
+        {
+          name: 'Fred Smith',
+          email: 'f.smith@gmail.com',
+          age: 56,
+          status: 'active',
+        },
+        {
+          name: 'Paul Newman',
+          email: 'p_newman@yahoo.com',
+          age: 23,
+          status: 'inactive',
+        },
+        {
+          name: 'Ted Johnson',
+          email: 'ted_johnson@gmail.com',
+          age: 37,
+          status: 'inactive',
+        },
+      ],
+    });
   
     console.log('Table "Users" has been created');
   } catch (err) {
